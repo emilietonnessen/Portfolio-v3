92cb46eeff0406d0aa76790affc173eada84737d
Emilie Tønnessen <emilie@tonnessen.com>
2022-12-22 12:41:50 +0100

    Add images to assets

---
+++ src/constants/images.ts
@@ -0,0 +1,11 @@
+import header from '../assets/header.jpg';
+import bgWhite from '../assets/bgWhite.png';
+import logo from '../assets/logo.png';
+
+const images = {
+  header,
+  bgWhite,
+  logo,
+};
+
+export default images;
