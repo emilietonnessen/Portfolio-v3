b33507f46b3347a922419665550b90d657571c21
Emilie Tønnessen <emilie@tonnessen.com>
2023-02-02 09:22:31 +0100

    Add loading and error handling to certificates

--- src/container/Certificates/Certificates.tsx
+++ src/container/Certificates/Certificates.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react';
-import { client, urlFor } from '../../client';
-import AppWrap from '../../wrapper/AppWrap';
-import MotionWrap from '../../wrapper/MotionWrap';
-import './Certificates.scss';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+
+import './Certificates.scss';
+import AppWrap from '../../wrapper/AppWrap';
+import ErrorBox from './../../components/ErrorBox/ErrorBox';
+import MotionWrap from '../../wrapper/MotionWrap';
+import Spinner from '../../components/Spinner/Spinner';
+import { client, urlFor } from '../../client';
 
 interface CertificatesProps {
   imgUrl: any;
@@ -14,44 +17,63 @@ interface CertificatesProps {
 
 const Certificates = () => {
   const [certificates, setCertificates] = useState<CertificatesProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   // Fetch Certificates
   useEffect(() => {
-    const query = '*[_type == "certificates"]';
+    if (!certificates.length) {
+      setIsLoading(true);
+      setIsError(false);
 
-    client.fetch(query).then((data: CertificatesProps[]) => {
-      setCertificates(data);
-    });
+      const query = '*[_type == "certificates"]';
+
+      client
+        .fetch(query)
+        .then((data: CertificatesProps[]) => {
+          setCertificates(data);
+          setIsLoading(false);
+        })
+        .catch(() => {
+          setIsError(true);
+          setIsLoading(false);
+        });
+    }
   }, []);
 
   return (
-    <div className="certificates">
+    <div className="certificates" aria-live="polite">
       <h2 className="head-text">Certificates</h2>
-      <ul className="certificates__list">
-        {certificates.map((certificate) => (
-          <motion.li
-            whileHover={{ scale: [1, 1.075] }}
-            transition={{ duration: 0.25 }}
-            key={certificate._id}
-            className="certificates__list-item"
-          >
-            <a
-              className="certificates__card"
-              href={certificate.link}
-              target="_blank"
-              rel="noreferrer"
-              title="Open link to course, new window"
+      {isLoading ? (
+        <Spinner />
+      ) : isError ? (
+        <ErrorBox message="Unable to load certificates. Please try again later." />
+      ) : (
+        <ul className="certificates__list">
+          {certificates.map((certificate) => (
+            <motion.li
+              whileHover={{ scale: [1, 1.075] }}
+              transition={{ duration: 0.25 }}
+              key={certificate._id}
             >
-              <img
-                src={urlFor(certificate.imgUrl).url()}
-                alt=""
-                role="presentation"
-              />
-              <h3 className="certificates__card-title">{certificate.title}</h3>
-            </a>
-          </motion.li>
-        ))}
-      </ul>
+              <a
+                className="certificates__card"
+                href={certificate.link}
+                target="_blank"
+                rel="noreferrer"
+                title="Open link to course, new window"
+              >
+                <img
+                  src={urlFor(certificate.imgUrl).url()}
+                  alt=""
+                  role="presentation"
+                />
+                <h3>{certificate.title}</h3>
+              </a>
+            </motion.li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
